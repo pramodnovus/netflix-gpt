aa8c1186d156e348e755f62bf40a27c0bfe8c79e
PranjalDevGoswami <pramod.kumar@novusinsights.com>
2024-09-07 14:50:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-400">
+      Namaste Everyone lets start the React Journey build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
